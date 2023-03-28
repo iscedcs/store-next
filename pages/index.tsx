bf48db2382d954d3a23fba10365907599acb69e4
Thebornsinner <Ozuzumatobe@gmail.com>
2023-03-28 17:11:18 +0100

    text config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,21 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import { LocationOn } from '@mui/icons-material';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { LocationOn } from "@mui/icons-material";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>ISCE Store</title>
-				<meta
-					content='ISCE Store UI'
-				/>
+				<meta content='ISCE Store UI' />
 				<meta
 					name='viewport'
 					content='width=device-width, initial-scale=1'
 				/>
-				<link
-					rel='icon'
-					href='/logo.ico'
-				/>
+				<link rel='icon' href='/logo.ico' />
 			</Head>
 			<main>
 				<div>
@@ -35,53 +30,70 @@ export default function Home() {
 					<LocationOn />
 				</div>
 
-				<div className="grid grid-cols-4">
-					<div className="">
-						<div className="bg-tertiary-100 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-200 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-300 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-400 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-500 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-600 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-700 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-800 h-10 w-[300px]"></div>
-						<div className="bg-tertiary-900 h-10 w-[300px]"></div>
+				<div className='grid grid-cols-4 mb-20'>
+					<div className=''>
+						<div className='bg-tertiary-100 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-200 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-300 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-400 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-500 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-600 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-700 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-800 h-10 w-[300px]'></div>
+						<div className='bg-tertiary-900 h-10 w-[300px]'></div>
 					</div>
-					<div className="">
-						<div className="bg-primary-100 h-10 w-[300px]"></div>
-						<div className="bg-primary-200 h-10 w-[300px]"></div>
-						<div className="bg-primary-300 h-10 w-[300px]"></div>
-						<div className="bg-primary-400 h-10 w-[300px]"></div>
-						<div className="bg-primary-500 h-10 w-[300px]"></div>
-						<div className="bg-primary-600 h-10 w-[300px]"></div>
-						<div className="bg-primary-700 h-10 w-[300px]"></div>
-						<div className="bg-primary-800 h-10 w-[300px]"></div>
-						<div className="bg-primary-900 h-10 w-[300px]"></div>
+					<div className=''>
+						<div className='bg-primary-100 h-10 w-[300px]'></div>
+						<div className='bg-primary-200 h-10 w-[300px]'></div>
+						<div className='bg-primary-300 h-10 w-[300px]'></div>
+						<div className='bg-primary-400 h-10 w-[300px]'></div>
+						<div className='bg-primary-500 h-10 w-[300px]'></div>
+						<div className='bg-primary-600 h-10 w-[300px]'></div>
+						<div className='bg-primary-700 h-10 w-[300px]'></div>
+						<div className='bg-primary-800 h-10 w-[300px]'></div>
+						<div className='bg-primary-900 h-10 w-[300px]'></div>
 					</div>
-					<div className="">
-						
-						<div className="bg-secondary-100 h-10 w-[300px]"></div>
-						<div className="bg-secondary-200 h-10 w-[300px]"></div>
-						<div className="bg-secondary-300 h-10 w-[300px]"></div>
-						<div className="bg-secondary-400 h-10 w-[300px]"></div>
-						<div className="bg-secondary-500 h-10 w-[300px]"></div>
-						<div className="bg-secondary-600 h-10 w-[300px]"></div>
-						<div className="bg-secondary-700 h-10 w-[300px]"></div>
-						<div className="bg-secondary-800 h-10 w-[300px]"></div>
-						<div className="bg-secondary-900 h-10 w-[300px]"></div>
+					<div className=''>
+						<div className='bg-secondary-100 h-10 w-[300px]'></div>
+						<div className='bg-secondary-200 h-10 w-[300px]'></div>
+						<div className='bg-secondary-300 h-10 w-[300px]'></div>
+						<div className='bg-secondary-400 h-10 w-[300px]'></div>
+						<div className='bg-secondary-500 h-10 w-[300px]'></div>
+						<div className='bg-secondary-600 h-10 w-[300px]'></div>
+						<div className='bg-secondary-700 h-10 w-[300px]'></div>
+						<div className='bg-secondary-800 h-10 w-[300px]'></div>
+						<div className='bg-secondary-900 h-10 w-[300px]'></div>
 					</div>
-					<div className="">
-						
-						<div className="bg-white h-10 w-[300px] mb-8"></div>
+					<div className=''>
+						<div className='bg-white h-10 w-[300px] mb-8'></div>
 
-						<div className="bg-info h-10 w-[300px]"></div>
-						<div className="bg-warning h-10 w-[300px]"></div>
-						<div className="bg-success h-10 w-[300px]"></div>
-						<div className="bg-error h-10 w-[300px]"></div>
-
+						<div className='bg-info h-10 w-[300px]'></div>
+						<div className='bg-warning h-10 w-[300px]'></div>
+						<div className='bg-success h-10 w-[300px]'></div>
+						<div className='bg-error h-10 w-[300px]'></div>
 					</div>
 				</div>
-				
+
+				<div className='grid grid-cols-4'>
+					<div className=''>
+						<div className='m-8'>
+							<div className='text-heading-lg'>
+								Text Heading Large
+							</div>
+							<div className='text-heading'>
+								Text Heading Medium
+							</div>
+						</div>
+						<div className='m-8'>
+							<div className='text-title-lg'>
+								Text Title Large
+							</div>
+							<div className='text-title'>
+								Text Title Medium
+							</div>
+						</div>
+					</div>
+				</div>
 			</main>
 		</>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'white': '#FFFFFF',
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				white: "#FFFFFF",
 
-        'info': '#0016DD',
-        'warning': '#DD7700',
-        'success': '#00B96B',
-        'error': '#BA1A1A',
-        
-        'secondary': {
-          '100': '#D0D3DC',
-          '200': '#B1B5C5',
-          '300': '#8A90A8',
-          '400': '#636C8B',
-          '500': '#3C476E',
-          '600': '#121C44',
-          '700': '#0E1736',
-          '800': '#070B1B',
-          '900': '#040710',
-        },
-        'primary': {
-          '100': '#CCCCCC',
-          '200': '#AAAAAA',
-          '300': '#808080',
-          '400': '#555556',
-          '500': '#2B2B2B',
-          '600': '#000000',
-        },
-        'tertiary': {
-          '100': '#CCE4FF',
-          '200': '#AAD3FF',
-          '300': '#80BCFF',
-          '400': '#55A6FF',
-          '500': '#2B90FF',
-          '600': '#0066D4',
-          '700': '#0051AA',
-          '800': '#003D80',
-          '900': '#003D80',
-        },
-      },
-    },
-  },
-  plugins: [],
-}
+				info: "#0016DD",
+				warning: "#DD7700",
+				success: "#00B96B",
+				error: "#BA1A1A",
+
+				secondary: {
+					100: "#D0D3DC",
+					200: "#B1B5C5",
+					300: "#8A90A8",
+					400: "#636C8B",
+					500: "#3C476E",
+					600: "#121C44",
+					700: "#0E1736",
+					800: "#070B1B",
+					900: "#040710",
+				},
+				primary: {
+					100: "#CCCCCC",
+					200: "#AAAAAA",
+					300: "#808080",
+					400: "#555556",
+					500: "#2B2B2B",
+					600: "#000000",
+				},
+				tertiary: {
+					100: "#CCE4FF",
+					200: "#AAD3FF",
+					300: "#80BCFF",
+					400: "#55A6FF",
+					500: "#2B90FF",
+					600: "#0066D4",
+					700: "#0051AA",
+					800: "#003D80",
+					900: "#003D80",
+				},
+			},
+			fontSize: {
+				"heading-lg": ["36px", "52px"],
+				"heading-md": ["28px", "40px"],
+			},
+		},
+	},
+	plugins: [],
+};
